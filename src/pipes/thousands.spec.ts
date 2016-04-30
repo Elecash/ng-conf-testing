d0ddf302c1f8c4a30a98b967a6cb2cb842e49a68
Raul <elecash@gmail.com>
2016-04-30 21:00:48 +0200

    Added all tests for Thousands pipe.

--- src/pipes/thousands.spec.ts
+++ src/pipes/thousands.spec.ts
@@ -13,4 +13,28 @@ describe('Thousands pipe tests', () => {
 
         expect(result).toEqual('300');
     });
+
+    it('Should transform 1300 to 1.3K', () => {
+        var result = pipe.transform('1300', null);
+
+        expect(result).toEqual('1.3K');
+    });
+
+    it('Should transform 13000 to 13K', () => {
+        var result = pipe.transform('13000', null);
+
+        expect(result).toEqual('13K');
+    });
+
+    it('Should transform 13500 to 13.5K', () => {
+        var result = pipe.transform('13500', null);
+
+        expect(result).toEqual('13.5K');
+    });
+
+    it('Should transform 13599 to 13.6K', () => {
+        var result = pipe.transform('13599', null);
+
+        expect(result).toEqual('13.6K');
+    });
 });
