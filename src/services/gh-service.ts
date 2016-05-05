1b761adbb9dc5636a3eca2f800ca6c4c77ad7704
Raul <elecash@gmail.com>
2016-05-05 21:45:32 +0200

    Added github client_id and client_secret.

--- src/services/gh-service.ts
+++ src/services/gh-service.ts
@@ -5,6 +5,7 @@ import {GithubUser} from './gh-user';
 
 export class GithubService {
     static GITHUB_API:string = 'https://api.github.com';
+    static GITHUB_PARAMS:string = '?client_id=aaa27ad06049163bd846&client_secret=d478f87d9bdf95efa72015b77b26c5b6a17127a0';
     http:Http;
     user:GithubUser;
 
@@ -13,13 +14,13 @@ export class GithubService {
     }
 
     getUserByUsername(username:string):Observable<any> {
-        let url:string = GithubService.GITHUB_API + '/users/' + username;
+        let url:string = GithubService.GITHUB_API + '/users/' + username + GithubService.GITHUB_PARAMS;
 
         return this.http.get(url).map(response => response.json());
     }
 
     getRepository(repo:string):Observable<any> {
-        let url:string = GithubService.GITHUB_API + '/repos/' + repo;
+        let url:string = GithubService.GITHUB_API + '/repos/' + repo + GithubService.GITHUB_PARAMS;
 
         return this.http.get(url).map(response => response.json());
     }
